7ecadc6c6d59877bc0d6e2b8d2f8fc2ff609d9be
omedcodes <omedsafi21@gmail.com>
2026-02-28 15:18:52 +0100

    Feat: implement draw circle function and new radius macro

--- paint.c
+++ paint.c
@@ -1,11 +1,35 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <SDL2/SDL.h>
+#include <math.h>
 
 #define WIDTH 900
 #define HEIGHT 600
+#define START_RADIUS 20
 
 #define TARGET_FPS 60
+
+void draw_circle(SDL_Surface *surface, int x_center, int y_center, int radius, Uint32 color)
+{
+    SDL_Rect pixel = {0, 0, 1, 1};
+    for (int x = x_center - radius; x < x_center + radius; x++) 
+    {
+        for (int y = y_center - radius; y < y_center + radius; y++) 
+        {
+            /* is this pixel part of the circle?
+               Top left corner is (x-r, y-r) and bottom right corner is (x+r, y+r)
+               distance from center and radius border sqrt( pow(x - x_center, 2) + pow(y - y_center, 2))
+            */
+            double distance_from_center = sqrt(pow(x - x_center, 2) + pow(y - y_center, 2));
+            if (distance_from_center < radius) 
+            {
+                pixel.x = x;
+                pixel.y = y;
+                SDL_FillRect(surface, &pixel, color);
+            }
+        }
+    }
+}
 
 int main(int argc, char *argv[]) 
 {
@@ -22,6 +46,7 @@ int main(int argc, char *argv[])
 
     float delay_milliseconds = (1.0f / TARGET_FPS) * 1000;
     int x, y;
+    int radius = START_RADIUS;
     while (!done) 
     {
         SDL_Event event;
@@ -40,8 +65,7 @@ int main(int argc, char *argv[])
             }
         }
         if (draw) {
-            SDL_Rect rect = {x, y, 100, 100};
-            SDL_FillRect(surface, &rect, 0x00FF0000);
+            draw_circle(surface, x, y, radius, 0x00FF0000);
             SDL_UpdateWindowSurface(window);
             SDL_Delay(delay_milliseconds);
         }
