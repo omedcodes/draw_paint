22c095497b09d396068ace8097d153a035f5d92a
omedcodes <omedsafi21@gmail.com>
2026-03-01 14:03:02 +0100

    Feat Huge progress on makefile and add icon + better software distribution

--- Makefile
+++ Makefile
@@ -1,23 +1,32 @@
 CC = gcc
+WINDRES = windres
 CFLAGS = -std=c23 -Wall -Wextra
-LDFLAGS = -lmingw32 -lSDL2main -lSDL2 -lm
+LDFLAGS_BASE = -lmingw32 -lSDL2main -lSDL2 -lm
+TARGET = DrawPaint
+SRC = main.c paint.c
+RES_RC = resource.rc
+RES_O = resource.res
 
-TARGET = main_paint
-SRC = paint.c
+.PHONY: all debug release clean check_dirs
 
-.PHONY: all debug release clean
+check_dirs:
+	@if not exist "assets" mkdir "assets"
 
-all: $(TARGET)
+all: check_dirs $(TARGET)
 
 debug: CFLAGS += -g
 debug: $(TARGET)
 
-release: CFLAGS += -O2 -mwindows
-release: LDFLAGS += -static
-release: $(TARGET)
+release: CFLAGS += -O2
+release: LDFLAGS_RELEASE = $(LDFLAGS_BASE) -mwindows -Wl,--dynamicbase -Wl,--nxcompat -lm -ldinput8 -ldxguid -ldxerr8 -luser32 -lgdi32 -lwinmm -limm32 -lole32 -loleaut32 -lshell32 -lsetupapi -lversion -luuid -static
+release: $(RES_O) $(TARGET)
+
+$(RES_O): $(RES_RC)
+	$(WINDRES) $(RES_RC) -O coff -o $(RES_O)
 
 $(TARGET): $(SRC)
-	$(CC) $(CFLAGS) $(SRC) -o $(TARGET) $(LDFLAGS)
+	$(CC) $(CFLAGS) $(SRC) $(wildcard $(RES_O)) -o $(TARGET) $(if $(findstring release,$(MAKECMDGOALS)),$(LDFLAGS_RELEASE),$(LDFLAGS_BASE))
 
 clean:
 	if exist $(TARGET).exe del $(TARGET).exe
+	if exist *.res del *.res
--- main.c
+++ main.c
@@ -3,34 +3,69 @@
 
 int main(int argc, char *argv[]) {
     (void)argc; (void)argv;
-    SDL_Init(SDL_INIT_VIDEO);
 
-    SDL_Window *window = SDL_CreateWindow("Draw Paint", SDL_WINDOWPOS_CENTERED, 
-                                          SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, 0);
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window *window = SDL_CreateWindow("Draw Paint", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN | SDL_WINDOW_MAXIMIZED | SDL_WINDOW_RESIZABLE);
+
     SDL_Surface *surface = SDL_GetWindowSurface(window);
+    SDL_FillRect(surface, NULL, 0xFFFFFFFF);
 
     Uint32 current_color = START_COLOR;
     Uint32 palette[8] = {0x000000, 0xFFFFFF, 0xFF0000, 0x00FF00, 0x0000FF, 0xFFFF00, 0x00FFFF, 0xFF00FF};
     
     bool done = false;
     bool draw = false;
-    int x, y, brush_size = START_RADIUS;
+    int x = 0, y = 0;
+    int brush_size = START_RADIUS;
     float delay = (1.0f / TARGET_FPS) * 1000;
 
     while (!done) {
         SDL_Event event;
         while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) done = true;
-            if (event.type == SDL_MOUSEMOTION) { x = event.motion.x; y = event.motion.y; }
-            if (event.type == SDL_MOUSEBUTTONDOWN) {
-                if (inside_palette(event.button.x, event.button.y))
-                    change_color(event.button.x, event.button.y, &current_color, palette);
-                else draw = true;
-            }
-            if (event.type == SDL_MOUSEBUTTONUP) draw = false;
-            if (event.type == SDL_MOUSEWHEEL) {
-                brush_size += event.wheel.y;
-                if (brush_size < 1) brush_size = 1;
+            switch (event.type) {
+                case SDL_QUIT:
+                    done = true;
+                    break;
+
+                case SDL_MOUSEMOTION:
+                    x = event.motion.x;
+                    y = event.motion.y;
+                    break;
+
+                case SDL_MOUSEBUTTONDOWN:
+                    if (inside_palette(event.button.x, event.button.y)) {
+                        change_color(event.button.x, event.button.y, &current_color, palette);
+                    } else {
+                        draw = true;
+                        draw_circle(surface, event.button.x, event.button.y, brush_size, current_color);
+                        SDL_UpdateWindowSurface(window);
+                    }
+                    break;
+
+                case SDL_MOUSEBUTTONUP:
+                    draw = false;
+                    break;
+
+                case SDL_MOUSEWHEEL:
+                    brush_size += event.wheel.y;
+                    if (brush_size < 1) brush_size = 1;
+                    break;
+
+                case SDL_KEYDOWN:
+                    if (event.key.keysym.sym == SDLK_s) {
+                        SDL_SaveBMP(surface, "painting.bmp");
+                    }
+                    if (event.key.keysym.sym == SDLK_c) {
+                        SDL_FillRect(surface, NULL, 0xFFFFFFFF);
+                        SDL_UpdateWindowSurface(window);
+                    }
+                    break;
+                
+                case SDL_WINDOWEVENT:
+                    if (event.window.event == SDL_WINDOWEVENT_RESIZED) {
+                        surface = SDL_GetWindowSurface(window);
+                    }
+                    break;
             }
         }
 
@@ -38,6 +73,7 @@ int main(int argc, char *argv[]) {
             draw_circle(surface, x, y, brush_size, current_color);
             SDL_UpdateWindowSurface(window);
         }
+
         draw_palette(surface, palette, 8);
         SDL_Delay(delay);
     }
